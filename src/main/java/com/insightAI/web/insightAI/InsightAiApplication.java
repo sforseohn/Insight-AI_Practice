47c58452ced33dfe821f91f3c2a61a69233cee75
sforseohn <sforseohn@gmail.com>
2023-06-17 02:34:17 +0900

    간단한 Test 작성

--- src/main/java/com/insightAI/web/insightAI/InsightAiApplication.java
+++ src/main/java/com/insightAI/web/insightAI/InsightAiApplication.java
@@ -2,6 +2,7 @@ package com.insightAI.web.insightAI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class InsightAiApplication {
@@ -9,5 +10,6 @@ public class InsightAiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InsightAiApplication.class, args);
 	}
+
 
 }
--- src/test/java/com/insightAI/web/insightAI/InsightAiApplicationTests.java
+++ src/test/java/com/insightAI/web/insightAI/InsightAiApplicationTests.java
@@ -2,12 +2,16 @@ package com.insightAI.web.insightAI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
+@RestController
 class InsightAiApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>Hello Insight-AI</h1>";
 	}
 
 }
